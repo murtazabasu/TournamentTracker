3e9cdbf3c10bec7af4b4beef37358e4d751227c1
murtazabasu <murtazab22@gmail.com>
2021-02-02 12:10:12 +0100

    Add project files.

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents one person.
+    /// </summary>
+    public class PersonModel
+    {
+        /// <summary>
+        /// The first name of the person.
+        /// </summary>
+        public string FirstName {get; set; }
+        
+        /// <summary>
+        /// The last name of the person.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The primary email address of the person.
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// The primary cell phone number of the peron.
+        /// </summary>
+        public string CellPhoneNumber { get; set; }
+    }
+}
